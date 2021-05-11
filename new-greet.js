16f341f7681ce0db5663e2d83b48f68d72769970
Ofhani-Dev <omukwevho3@gmail.com>
2021-05-11 15:27:55 +0200

    figlet changes

--- new-greet.js
+++ new-greet.js
@@ -1,7 +1,18 @@
 const chalk = require("chalk");
-
 const greet = require("./greet");
+const figlet = require("figlet");
 
 const styledMessage = chalk.bgGreen.black(greet("Ofhani"))
-
 console.log(greet(styledMessage));
+
+const message = greet("Ofhani");
+
+figlet(message, function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    const styledMessage = chalk.bgGreen.black(data);
+    console.log(styledMessage)
+});
